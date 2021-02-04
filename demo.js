d883cd89b0187c52954c1dacb0880a1d9a353f3f
root <root@ip-10-0-0-53.ap-south-1.compute.internal>
2021-02-04 12:12:36 +0000

    second commit

--- demo.js
+++ demo.js
@@ -1,8 +1,35 @@
-let http = require('http')
-server = http.createServer(function(request, response) {
-    response.write('Hello yo')
-    response.end()
-})
+//include http module
+var http = require('http');
 
-server.listen(8082)
-console.log ("Server Running")
+http.createServer(function(req,res){
+        //store URL in variable q_string
+
+        var q_string = req.url;
+        switch(q_string) {
+                case '/':
+                                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                                res.write('Welcome To GlistenSoft Demo!')
+                                res.end();
+                                break;
+                        case '/about':
+                                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                                res.write('About Us');
+                                res.write('\n\n');
+                                res.write('Glisten Software Pvt Ltd. Baner, Pune');
+                                res.write('\n');
+                                res.end('Find out more: https://www.glistensoft.com/');
+                                break;
+                        case '/tecmint/authors':
+                                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                                res.write('Tecmint Authors');
+                                res.write('\n\n');
+                                res.end('Find all our authors here: https://www.tecmint.com/who-we-are/');
+                                break;
+                        default:
+                                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                                res.end('Not Found');
+                                break;
+        }
+}).listen(3333);
+console.log('Server started on localhost:3333; press Ctrl-C to terminate....');
+
